b365eb50c18f1e600e6035075419514ed34dff0d
oscarl125 <oscar.corral125@gmail.com>
2025-07-28 16:51:07 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>VendoName</h1>
+      <p>Welcome to the decentralized domain vending machine!</p>
+    </div>
+  );
+}
+
+export default App;
